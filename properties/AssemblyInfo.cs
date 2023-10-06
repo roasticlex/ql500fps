46bfe30230f419be2e294622f664cc44d394b13a
roasticlex <62638338+roasticlex@users.noreply.github.com>
2023-10-06 12:00:39 -0300

    Add files via upload

---
+++ properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("QuakeLive500FPS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuakeLive500FPS")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("da0fdb4d-b121-4717-bfa3-79e78ab19c04")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
